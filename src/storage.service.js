647f9a13b1f8a7c471269045a58f8d2a3b7af119
Kathode <work.qiyan@gmail.com>
2025-02-08 20:25:30 +1100

    Add storage service class to fetch & update data

---
+++ src/storage.service.js
@@ -0,0 +1,13 @@
+class StorageService {
+  constructor(key) {
+    this.key = key;
+  }
+
+  save(data) {
+    localStorage.setItem(this.key, JSON.stringify(data));
+  }
+
+  load() {
+    return JSON.parse(localStorage.getItem(this.key)) || [];
+  }
+}
