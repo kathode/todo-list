173842247c7f29c64908dfb28ee392cb233ef6ff
Kathode <work.qiyan@gmail.com>
2025-02-10 22:38:27 +1100

    Apply styling depending on task status

--- src/todo.ui.js
+++ src/todo.ui.js
@@ -1,15 +1,26 @@
 import { createElement } from "./helper";
 
-export const createTodoItem = (data, ToDoList) => {
+export const displayTodoItem = (data, TodoClass) => {
   const checkbox = createElement("input", { className: "todo-checkbox", type: "checkbox", checked: data.isComplete });
-  const titleDisplay = createElement("div", { className: "todo-title", innerText: data.title });
+  const titleDisplay = createElement("div", { className: `todo-title  ${data.isComplete ? "todo-complete" : ""}`, innerText: data.title });
   const editButton = createElement("button", { className: "todo-edit-button", innerText: "View" });
   const dueDateDisplay = createElement("div", { className: "todo-due-date", innerText: data.dueDate });
   const removeButton = createElement("button", { className: "todo-remove-button", innerText: "Remove" });
 
   removeButton.addEventListener("click", () => {
-    ToDoList.removeTodo(data.id);
+    TodoClass.removeTodo(data.id);
     removeButton.closest(".todo-item").remove();
+  });
+
+  checkbox.addEventListener("click", (va) => {
+    const isComplete = va.target.checked;
+    TodoClass.completeTodo(data.id, isComplete);
+
+    if (isComplete) {
+      titleDisplay.classList.add("todo-complete");
+    } else {
+      titleDisplay.classList.remove("todo-complete");
+    }
   });
 
   const column1 = createElement("div", { className: "todo-wrapper" }, checkbox, titleDisplay);
