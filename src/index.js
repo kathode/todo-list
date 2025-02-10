2a716872ff7e17bff99bf571adad55aec3634339
Kathode <work.qiyan@gmail.com>
2025-02-10 21:58:15 +1100

    Display the todo items and add hardcoded tasks when clicked

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+import { ToDoList } from "./logic";
+import { LocalStorageService } from "./localStorage.service";
 import "./styles.css";
+import { displayTodoItem } from "./todo.ui";
+
+const button = document.querySelector(".new-todo-button");
+
+(function () {
+  const todos = new ToDoList(new LocalStorageService());
+
+  for (const todo of todos.getTodos()) {
+    displayTodoItem(todo, todos);
+  }
+
+  // create hardcoded todo tasks for now
+  button.addEventListener("click", () => {
+    const newTodo = {
+      id: todos.getIdCounter(),
+      title: "abc",
+      description: "description",
+      notes: "notes",
+      dueDate: "3 December",
+      priority: "high",
+      isComplete: false,
+    };
+
+    displayTodoItem(newTodo, todos);
+    todos.addTodo(newTodo);
+  });
+})();
