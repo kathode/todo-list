e259c336fb2c298ac1c9ae0f0e37e8210b2b83a6
Kathode <work.qiyan@gmail.com>
2025-02-25 17:10:50 +1100

    Remove this.array

--- src/project.logic.js
+++ src/project.logic.js
@@ -3,6 +3,5 @@ import { BaseClass } from "./base.logic";
 export class Project extends BaseClass {
   constructor(storageService) {
     super(storageService);
-    this.projects = this.storageService.load();
   }
 }
