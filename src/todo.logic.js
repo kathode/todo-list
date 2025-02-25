01da228d26e2a4819090dfb6dc2c608481fd55e0
Kathode <work.qiyan@gmail.com>
2025-02-25 17:11:18 +1100

    Remove this.array

--- src/todo.logic.js
+++ src/todo.logic.js
@@ -4,15 +4,14 @@ import { BaseClass } from "./base.logic";
 export class Todo extends BaseClass {
   constructor(storageService) {
     super(storageService);
-    this.todos = this.storageService.load();
   }
 
   getTodoType(type) {
     switch (type) {
       case "ALL":
-        return this.todos;
+        return this.array;
       case "TODAY":
-        return this.todos.filter((todo) => format(new Date(todo.dueDate), "EEE, dd MMM yyy") === format(new Date(), "EEE, dd MMM yyy"));
+        return this.array.filter((todo) => format(new Date(todo.dueDate), "EEE, dd MMM yyy") === format(new Date(), "EEE, dd MMM yyy"));
     }
   }
 }
